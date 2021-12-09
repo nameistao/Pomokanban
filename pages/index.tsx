acfad250eacb602b0799349b9fb3f868f3c9b0b4
tao luo <taoluo@usc.edu>
2021-12-08 18:07:47 -0800

    remove dummy tasks

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,25 +43,12 @@ const Home: NextPage = () => {
   //show modals
   const [showModal, setShowModal] = useState("");
   const [taskData, setTaskData] = useState({
-    tasks: {
-      "task-1": {
-        id: "task-1",
-        content: "take out garbage",
-      },
-      "task-2": {
-        id: "task-2",
-        content: "watch fav show",
-      },
-      "task-3": {
-        id: "task-3",
-        content: "take out garbage",
-      },
-    },
+    tasks: {},
     columns: {
       "column-1": {
         id: "column-1",
         title: "To Do",
-        taskIds: ["task-1", "task-2", "task-3"],
+        taskIds: [],
       },
       "column-2": {
         id: "column-2",
