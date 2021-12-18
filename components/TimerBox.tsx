55036b33a225080fd4b12cd98687e30c72aa5c92
tao luo <taoluo@usc.edu>
2021-12-18 12:17:57 -0800

    add initial media queries

--- components/TimerBox.tsx
+++ components/TimerBox.tsx
@@ -10,7 +10,7 @@ import Repeat from "components/icons/Repeat";
 
 const StyledSection = styled.section`
   height: 32.5vh;
-  width: 50vw;
+  width: 60vw;
   padding: 0 4vw 0 4vw;
   color: #fff;
   margin: 0 auto 2vh auto;
@@ -19,6 +19,12 @@ const StyledSection = styled.section`
   display: flex;
   flex-direction: column;
   transition: background-color 0.7s ease;
+  @media (max-width: 1024px) {
+    width: 90vw;
+  }
+  @media (max-width: 480px) {
+    width: 95vw;
+  }
 `;
 
 const TimerBox = () => {
